d5182687d5a6e3025a69deaa2ab23a45ef72a31b
cctong-castiel <castieltong@gmail.com>
2025-10-19 14:50:18 +0800

    start explore transformer evaluation dev code

--- Labfiles/01-prebuild-models/Python/document-analysis.py
+++ Labfiles/01-prebuild-models/Python/document-analysis.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+import os
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 
+load_dotenv()
+
 # Store connection information
-endpoint = "<Endpoint URL>"
-key = "<API Key>"
+endpoint = os.getenv("DOCUMENT_INTELLIGENCE_ENDPOINT")
+key = os.getenv("DOCUMENT_INTELLIGENCE_KEY")
 
 fileUri = "https://github.com/MicrosoftLearning/mslearn-ai-document-intelligence/blob/main/Labfiles/01-prebuild-models/sample-invoice/sample-invoice.pdf?raw=true"
 fileLocale = "en-US"
@@ -13,14 +17,23 @@ print(f"\nConnecting to Forms Recognizer at: {endpoint}")
 print(f"Analyzing invoice at: {fileUri}")
 
 # Create the client
+document_intelligence_client  = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
 
 # Analyse the invoice
+poller = document_intelligence_client.begin_analyze_document_from_url(
+    model_id=fileModelId,
+    document_url=fileUri,
+    locale=fileLocale
+)
 
 # Display invoice information to the user
+result = poller.result()
 
 
-
-
+for idx, receipt in enumerate(result.documents):
+    print(f"\n-------- Analyzing invoice #{idx + 1} --------")
     customer_name = receipt.fields.get("CustomerName")
     if customer_name:
         print(f"Customer Name: '{customer_name.value}, with confidence {customer_name.confidence}.")
